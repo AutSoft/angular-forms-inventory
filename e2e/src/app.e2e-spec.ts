49d49db21720073c1e5d0235ad2c51adffc3620f
szuperaz <szuperaz@gmail.com>
2020-01-15 22:37:37 +0100

    Complete task 17 - create item test

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,5 +1,6 @@
-import { AppPage } from './app.po';
 import { browser, logging } from 'protractor';
+import { Currency, Item } from '../../src/app/api/inventory.generated';
+import { AppPage } from './app.po';
 
 describe('workspace-project App', () => {
   let page: AppPage;
@@ -23,6 +24,26 @@ describe('workspace-project App', () => {
     await browser.waitForAngular();
 
     expect(await page.getItems().count()).toBe(numberOfItems - 1);
+  });
+
+  it('should create item', async () => {
+    const numberOfItems = await page.getItems().count();
+
+    await page.getCreateButton().click();
+    const item = new Item({
+      name: 'MacBook Pro 2018',
+      type: 'laptop',
+      description: 'MacBook Pro with Retina display',
+      count: 3,
+      countDate: new Date(),
+      dimension: { width: 10, height: 5, depth: 0.5 },
+      price: { value: 10, currency: Currency.USD }
+    });
+    await page.fillForm(item);
+    await page.getSubmitButton().click();
+    await browser.waitForAngular();
+
+    expect(await page.getItems().count()).toBe(numberOfItems + 1);
   });
 
   afterEach(async () => {
--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -1,4 +1,5 @@
-import { browser, by, element } from 'protractor';
+import { browser, by, element, Key } from 'protractor';
+import { Currency, Item } from '../../src/app/api/inventory.generated';
 
 export class AppPage {
   navigateTo() {
@@ -11,5 +12,31 @@ export class AppPage {
 
   getDeleteButton(index: number) {
     return element.all(by.css('[mattooltip = "Delete"]')).get(index);
+  }
+
+  getCreateButton() {
+    return element(by.partialButtonText('New item'));
+  }
+
+  getSubmitButton() {
+    return element(by.partialButtonText('Save'));
+  }
+
+  async fillForm(item: Item) {
+    await element(by.css('[placeholder = "Name"]')).sendKeys(item.name);
+    await element(by.css('[placeholder = "Type"]')).sendKeys(item.type);
+    await element(by.css('[placeholder = "Description"]')).sendKeys(item.description);
+
+    await element(by.cssContainingText('.mat-tab-label-content', 'Details')).click();
+
+    await element(by.css('[placeholder = "Count"]')).sendKeys(item.count);
+    await element(by.css('[placeholder = "Count Date"]')).sendKeys(item.countDate.toISOString());
+    await element(by.css('mat-slide-toggle')).sendKeys(Key.SPACE);
+    await element(by.css('[placeholder = "Width"]')).sendKeys(item.dimension.width);
+    await element(by.css('[placeholder = "Height"]')).sendKeys(item.dimension.height);
+    await element(by.css('[placeholder = "Depth"]')).sendKeys(item.dimension.depth);
+    await element(by.css('[placeholder = "Price"]')).sendKeys(item.price.value);
+    await element(by.css('[name = "Currency"]')).click();
+    await element(by.cssContainingText('span', Currency[item.price.currency])).click();
   }
 }
