bf02f2f359d616ed72dcbf2d2934af170c1d7b55
szuperaz <szuperaz@gmail.com>
2020-01-15 19:44:06 +0100

    Configure Protractor e2e test env

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -5,12 +5,16 @@ describe('workspace-project App', () => {
   let page: AppPage;
 
   beforeEach(() => {
-    page = new AppPage();
+    return browser.executeAsyncScript(
+      // tslint:disable-next-line:only-arrow-functions
+      function(cb) { fetch('https://autsoftangulare2e.azurewebsites.net/api/Inventory/resetDatabase?password=123').then(cb); }
+    );
   });
 
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getTitleText()).toEqual('inventory app is running!');
+  it('should do nothing', () => {});
+
+  beforeEach(() => {
+    page = new AppPage();
   });
 
   afterEach(async () => {
