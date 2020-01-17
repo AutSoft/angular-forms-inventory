e8aa288fe1db0296b062c91116b75e19adebddd8
szuperaz <szuperaz@gmail.com>
2020-01-17 21:02:21 +0100

    Complete task 21 - add item test

--- cypress/integration/form.spec.js
+++ cypress/integration/form.spec.js
@@ -15,4 +15,40 @@ describe('Inventory tests', () => {
       cy.get(`table tbody tr`).should("have.length", prevList.length - 1);
     });
   });
+
+  it('should add new item', () => {
+    cy.visit('/');
+
+    cy.get('table tbody tr').then(prevList => {
+      cy.contains('New item').click();
+
+      cy.get('[placeholder=Name]').type('MacBook Pro 2018');
+      cy.get('[placeholder=Type]').type('laptop');
+      cy.get('[placeholder=Description]').type('MacBook Pro with Retina display');
+
+      cy.contains('Details').click();
+
+      cy.get('[placeholder=Count]').type(3);
+      cy.get(`[placeholder='Count Date']`)
+        .clear()
+        .type('2020/01/13');
+      cy.contains('Dimension data').click();
+      cy.get('[placeholder=Width]')
+        .clear()
+        .type(20);
+      cy.get('[placeholder=Height]')
+        .clear()
+        .type(30);
+      cy.get('[placeholder=Depth]')
+        .clear()
+        .type(0.5);
+      cy.get('[placeholder=Price]').type(10);
+      cy.get('[name=Currency]').click();
+      cy.contains('USD').click();
+
+      cy.contains('Save').click();
+
+      cy.get(`table tbody tr`).should('have.length', prevList.length + 1);
+    });
+  });
 });
